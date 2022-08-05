21ebf8d197fb7b907c83078a7477e32ec42c7aee
Ricardo Antonio de Souza <ricardo.souza@clickbus.com>
2022-08-05 12:17:59 -0400

    Violation of the SRP implemented

---
+++ src/main/java/solid/p1sr/violation/Costumer.java
@@ -0,0 +1,66 @@
+package solid.p1sr.violation;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
+@Getter
+@Setter
+public class Costumer {
+
+  private Long id;
+  private String name;
+  private String email;
+  private String phoneNumber;
+  private Date birthday;
+  private String registration;
+
+  public String addCostumer(Costumer costumer) throws SQLException, EmailException {
+    if (isBlank(costumer.getRegistration()) || costumer.getRegistration().length() != 11) {
+      throw new IllegalArgumentException("Invalid registration");
+    }
+    if (isBlank(costumer.getName())) {
+      throw new IllegalArgumentException("Invalid name");
+    }
+    if (isBlank(costumer.getEmail()) || !costumer.getEmail().contains("@")) {
+      throw new IllegalArgumentException("Invalid email");
+    }
+
+    try (
+      Connection conn = DriverManager.getConnection("url");
+      PreparedStatement ps = conn.prepareStatement("INSERT INTO costumer VALUES(?,?,?,?,?)");
+    ) {
+      ps.setString(1, costumer.getName());
+      ps.setString(2, costumer.getEmail());
+      ps.setString(3, costumer.getPhoneNumber());
+      ps.setDate(4, costumer.getBirthday());
+      ps.setString(5, costumer.getRegistration());
+
+      ps.executeUpdate();
+    }
+
+    Email email = new SimpleEmail();
+    email.setHostName("smtp.googlemail.com");
+    email.setSmtpPort(465);
+    email.setAuthenticator(new DefaultAuthenticator("username", "password"));
+    email.setFrom("user@gmail.com");
+    email.setSubject("TestMail");
+    email.setMsg("This is a test mail ... :-)");
+    email.addTo(costumer.getEmail());
+
+    email.send();
+
+    return "Sucessfully";
+  }
+}

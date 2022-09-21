a08caa639473a471684d75d0e46d1c7610e555e8
Ricardo Antonio de Souza <ricardo.souza@clickbus.com>
2022-09-21 16:57:59 -0400

    DIP implemented

--- src/test/java/solid/p5di/solution/CostumerServiceTest.java
+++ src/test/java/solid/p5di/solution/CostumerServiceTest.java
@@ -2,19 +2,20 @@ package solid.p5di.solution;
 
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 class CostumerServiceTest {
 
+  @Mock
   private Repository<Costumer> repository;
   @Mock
   private EmailSender sender;
 
+  @InjectMocks
   private ICostumerService service;
 
   @Test
   void test() {
-    repository = new CostumerMySqlRepository();
-    service = new CostumerService(repository, sender);
   }
 }
